66e2b4fb022d0fdc5c70c0bfbc544782f44a6cdc
tosunkaya <etosunkaya@hotmail.com>
2022-09-14 01:47:01 +0300

    Update siteConfig.js

--- src/utils/siteConfig.js
+++ src/utils/siteConfig.js
@@ -1,9 +1,6 @@
 const config = require(`../../.ghost.json`).production;
 module.exports = {
-    siteUrl:
-        process.env.NODE_ENV === `production`
-            ? process.env.SITE_URL || config.siteUrl || `http://localhost:8000`
-            : `https://ghost.tosunkaya.com`, // Site domain. Do not include a trailing slash!
+    siteUrl: `https://ghost.tosunkaya.com`, // Site domain. Do not include a trailing slash!
 
     postsPerPage: 12, // Number of posts shown on paginated pages (changes this requires sometimes to delete the cache)
 
